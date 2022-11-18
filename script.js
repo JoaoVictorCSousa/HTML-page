9a42aefd17fa18681a2cbb4c13f5960d6f7fc39f
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2022-11-18 10:49:23 -0300

    feat: adding javascript features

---
+++ script.js
@@ -0,0 +1,13 @@
+function handleClick() {
+    document.querySelector('#titulo').innerHTML = 'Coringa';
+}
+
+function handleMove() {
+    document.querySelector('#titulo').innerHTML = 'Você está morto!!';
+}
+
+function handleOut() {
+    document.querySelector('#titulo').innerHTML = 'Não entre em desespero!';
+}
+
+alert('Cuidado, você está entrando em algo que irá se arrepender!')
